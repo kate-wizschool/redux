c473279bde91348877c36f78504ea29329a79909
kate-wizschool <kate@wizschool.io>
2019-11-08 11:30:37 +0900

    counter reducer created

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,1 +1,19 @@
 // 카운터 관련 상태 로직
+const INCREMENT = "counter/INCREMENT";
+const DECREMENT = "counter/INCREMENT";
+
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+
+const initialState = {
+  number: 0
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return { number: state.number + 1 };
+    case DECREMENT:
+      return { number: state.number - 1 };
+  }
+}
